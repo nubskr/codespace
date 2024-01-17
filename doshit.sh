ed3c17697608bda4bae8daa00bfd8832f1ea8d8f
nubskr <dakshrinwan2003@gmail.com>
2024-01-17 18:18:08 +0530

    .

--- doshit.sh
+++ doshit.sh
@@ -2,4 +2,14 @@
 cd ./contest
 g++ a.cpp
 ./a.out < input.txt > output.txt
+# compare the file with the expected_output file then put the result in verdict file
+rm compare.txt
+rm verdict.txt
+diff output.txt expected_output.txt >> compare.txt
+# cat compare.txt
+if [[ -s compare.txt ]]; then
+    echo "WA" >> verdict.txt
+else
+    echo "AC" >> verdict.txt
+fi
 echo done
